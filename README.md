abd08660c4c9ab24ab49ef3f65d3ece0b1fc8b9c
vinc3nt <vinc3nt@riseup.net>
2013-08-21 19:35:01 -0400

    fixed some bugs; added 'info' and 'new' options; moved incomplete 'launch_tor_with_config' section in a separate brach

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 stem-tortp
 ==========
 
-TORtp enhancement with stem integration
+TORtp is a simple way to implement Tor Transparent Proxy in our GNU/Linux Box.
+
+TORtp use TOR's control port (python-stem library) for setup Transparen Proxy and DNS server capability on TOR, without override default configuration file with a custom /etc/tor/torrc
 
 WARNING: this project is in its initial state, the code is not yet tested
--- tort.py
+++ tort.py
@@ -2,8 +2,7 @@
 
 """
 TORtp is a simple way to implement
-Tor Transparent Proxy in our
-GNU/Linux Box
+Tor Transparent Proxy in our GNU/Linux Box
 
 TORtp use TOR's control port for setup
 Transparen Proxy and DNS server capability
@@ -19,15 +18,13 @@ import stem
 from stem.control import Controller
 import stem.util
 import stem.process
+from stem import CircStatus
 from shutil import copy2
 import sys
 
-# setting the process name
-stem.util.system.set_process_name("TORtp")
-
 def notify(title, message):
     pynotify.init("TORtp")
-    notice = pynotify.Notification(title, message, "./icon/anonymous.ico")
+    notice = pynotify.Notification(title, message, "/usr/share/icons/tortp/anonymous.ico")
     notice.show()
     return
 
@@ -55,6 +52,17 @@ def tortp_dir(home):
       notify("TORtp", "Directory %s created" % tortpdir)
    return tortpdir
 
+def check_sys_dependecies():
+   """ check if all dependencies are installed """
+   devnull = open(os.devnull,"w")
+   dnsmasq = subprocess.call(["dpkg","-s","dnsmasq"],stdout=devnull,stderr=subprocess.STDOUT)
+   if dnsmasq != 0:
+      print "Package dnsmasq not installed"
+   tor = subprocess.call(["dpkg","-s","tor"],stdout=devnull,stderr=subprocess.STDOUT)
+   if tor != 0:
+       print "Package tor not installed"
+   devnull.close()
+
 def iptables_clean():
    """ This function remove all iptables rules """
    subprocess.call('iptables -F', shell=True)
@@ -63,6 +71,7 @@ def iptables_clean():
    subprocess.call('iptables -t nat -X', shell=True)
 
 def iptables_up(tortpdir, user):
+   """ This function add iptables rules for redirect all user traffic to tortp """
    subprocess.call('iptables-save > %s/%s' % (tortpdir, "iptables.txt"), shell=True)
    # Redirect DNSTor port (9053)
    subprocess.call('iptables -t nat -A OUTPUT ! -o lo -p udp -m owner --uid-owner %s -m udp --dport 53 -j REDIRECT --to-ports 9053' % user, shell=True)
@@ -73,72 +82,115 @@ def iptables_up(tortpdir, user):
    subprocess.call('iptables -t filter -A OUTPUT -p tcp -m owner --uid-owner %s -m tcp --dport 9040 -j ACCEPT' % user, shell=True)
    subprocess.call('iptables -t filter -A OUTPUT ! -o lo -m owner --uid-owner %s -j DROP' % user, shell=True)
    subprocess.call('iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner %s -m tcp --syn -d 127.0.0.1 --dport 9051 -j ACCEPT' % user, shell=True)
-   notify("TORtp", "Tor DNS enabled")
 
-def iptables_down():
-   subprocess.call('iptables-restore < %s/%s' % (tortpdir, "iptables.txt"), shell=True)
+def iptables_down(tortpdir):
+   try:
+      subprocess.call('iptables-restore < %s/%s' % (tortpdir, "iptables.txt"), shell=True)
+   except IOError as e:
+      iptables_clean()
+      print e
 
 def resolvconf(tortpdir):
+   """ Backup and modify resolv configuration file """
    try:
       copy2("/etc/resolv.conf",tortpdir)
    except IOError as e:
       print e
    resolv = open('/etc/resolv.conf', 'w')
    resolv.write('nameserver 127.0.0.1\n')
+   resolv.close()
 
-def enable_dns_torify(tortpdir):
+def dnsmasq(tortpdir):
+   """ Backup and modify dnsmasq configuration file """
    try:
-      with Controller.from_port(port = 9051) as controller:
-         controller.authenticate()
-         controller.set_options({"DNSPort": "9053", "AutomapHostsOnResolve": "1"})
-         notify("TORtp", "Tor DNS enabled")
-   except stem.SocketError:
-      # WRONG: launch new tor process as a debian-tor and not as root
-      stem.process.launch_tor_with_config(config = {'ControlPort': '9051', 'CookieAuthentication': '1'},)
-      with Controller.from_port(port = 9051) as controller:
-         controller.authenticate()
-         controller.set_options({"DNSPort": "9053", "AutomapHostsOnResolve": "1"})
-         notify("TORtp", "Tor DNS enabled")
+      copy2("/etc/dnsmasq.conf",tortpdir)
+   except IOError as e:
+      print e
+   dmasq = open('/etc/dnsmasq.conf', 'w')
+   dmasq.write('no-resolv\n')
+   dmasq.write('server=127.0.0.1#9053\n')
+   dmasq.write('listen-address=127.0.0.1\n')
+
+def enable_tordns():
+   with Controller.from_port(port = 9051) as controller:
+      controller.authenticate()
+      controller.set_options({"DNSPort": "9053", "AutomapHostsOnResolve": "1", "AutomapHostsSuffixes": ".exit,.onion"})
 
 def enable_torproxy():
+   with Controller.from_port(port = 9051) as controller:
+      controller.authenticate()
+      controller.set_options({"VirtualAddrNetwork": "10.192.0.0/10", "TransPort": "9040", "TransListenAddress": "127.0.0.1","AvoidDiskWrites": "1", "WarnUnsafeSocks": "1"})
+
+def exit_info():
+   """ Print info about my exit node """
+   with Controller.from_port(port = 9051) as controller:
+      controller.authenticate()
+      for circ in controller.get_circuits():
+         if circ.status != CircStatus.BUILT:
+            continue
+         exit_fp, exit_nickname = circ.path[-1]
+         exit_desc = controller.get_network_status(exit_fp, None)
+         exit_address = exit_desc.address if exit_desc else 'unknown'
+         print "Exit relay"
+         print "  fingerprint: %s" % exit_fp
+         print "  nickname: %s" % exit_nickname
+         print "  address: %s" % exit_address
+
+def tor_new():
+   """ Create a new tor circuit """
    try:
-      with Controller.from_port(port = 9051) as controller:
-         controller.authenticate()
-         controller.set_options({"VirtualAddrNetwork": "10.192.0.0/10", "TransPort": "9040", "TransListenAddress": "127.0.0.1","AvoidDiskWrites": "1", "WarnUnsafeSocks": "1"})
-         notify("TORtp", "Tor Proxy enabled")
-   except stem.SocketError:
-       # WRONG: launch new tor process as a debian-tor and not as root
-       stem.process.launch_tor_with_config(config = {'ControlPort': '9051', 'CookieAuthentication': '1'},)
-       with Controller.from_port(port = 9051) as controller:
-          controller.authenticate()
-          controller.set_options({"VirtualAddrNetwork": "10.192.0.0/10", "TransPort": "9040", "TransListenAddress": "127.0.0.1","AvoidDiskWrites": "1", "WarnUnsafeSocks": "1"})
-          notify("TORtp", "Tor Proxy enabled")
+      stem.socket.ControlPort(port = 9051)
+   except stem.SocketError as exc:
+      print "Unable to connect to port 9051 (%s)" % exc
+      print "Please add 'ControlPort 9051' on your /etc/tor/torrc configuration"
+      sys.exit(1)
+   with Controller.from_port(port = 9051) as controller:
+      controller.authenticate()
+      controller.signal(stem.Signal.NEWNYM)
+      notify("TORtp", "New Tor circuit created")
 
 def start(tortpdir):
-   enable_dns_torify(tortpdir)
-   enable_torproxy()
-   resolvconf(tortpdir)
+   try:
+      stem.socket.ControlPort(port = 9051)
+   except stem.SocketError as exc:
+      print "Unable to connect to port 9051 (%s)" % exc
+      print "Please add 'ControlPort 9051' on your /etc/tor/torrc configuration"
+      sys.exit(1)
    iptables_clean()
    iptables_up(tortpdir, check_user())
+   enable_tordns()
+   enable_torproxy()
+   resolvconf(tortpdir)
+   dnsmasq(tortpdir)
+   subprocess.call('/etc/init.d/dnsmasq restart', shell=True)
+   notify("TORtp", "Tor Transparent Proxy enabled")
 
 def stop(tortpdir):
-   pass
-   # I'll have to write it
+   """ Restore all original files"""
+   try:
+      copy2("%s/resolv.conf" % tortpdir, "/etc")
+      copy2("%s/dnsmasq.conf" % tortpdir, "/etc")
+   except IOError as e:
+      print e
+   subprocess.call('/etc/init.d/dnsmasq restart', shell=True)
+   subprocess.call('/etc/init.d/tor reload', shell=True)
+   iptables_down(tortpdir)
+   notify("TORtp", "Tor Transparent Proxy disabled")
 
 def main(arg):
    parser = argparse.ArgumentParser()
-   parser.add_argument("do", help="start o stop?")
+   parser.add_argument("do", help="start | stop | new | info")
    args = parser.parse_args()
    if args.do == "start":
-      tortpdir = tortp_dir(set_home(check_user()))
-      start(tortpdir)
+      start(tortp_dir(set_home(check_user())))
    if args.do == "stop":
-      ortpdir = tortp_dir(set_home(check_user()))
-      stop()
-   if args.do != "start" and args.do != "stop":
+      stop(tortp_dir(set_home(check_user())))
+   if args.do == "new":
+      tor_new()
+   if args.do == "info":
+      exit_info()
+   if args.do != "start" and args.do != "stop" and args.do != "new" and args.do != "info":
       print 'Type "tortp.py -h" for options'
-
-   # check if tor is working
 
 if __name__ == '__main__':
    main(sys.argv[1:])
