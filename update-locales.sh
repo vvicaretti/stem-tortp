fd93199a393c5ad091f75c73050adc9b01dd5417
paskao <paskao@hacari.org>
2014-02-06 17:50:19 +0100

    don't put comments in locale files

--- update-locales.sh
+++ update-locales.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Update .po files
-find locale -name "*.po" -execdir xgettext -L Python -j -o tortp-gtk.po --package-name tortp-gtk --package-version 1.1 --msgid-bugs-address paskao@hacari.org `pwd`/tortp-gtk \;
+find locale -name "*.po" -execdir xgettext -L Python -j -o tortp-gtk.po --package-name tortp-gtk --package-version 1.1 --msgid-bugs-address paskao@hacari.org --no-location `pwd`/tortp-gtk \;
 find locale -name "*.po" -execdir msgfmt tortp-gtk.po -o tortp-gtk.mo \;
 
