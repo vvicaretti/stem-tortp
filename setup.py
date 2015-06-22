44661d957d0aab084c0687a0227bd90585212620
boyska <piuttosto@logorroici.org>
2015-06-22 15:38:26 +0200

    proper debian packaging!

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from distutils.core import setup
 setup(name='tortp',
         version='0.4',
         py_modules=['tortp'],
-        requires=['stem']
+        requires=['stem'],
+	scripts=['tortp-gtk'],
+	data_files=[('share/applications/', ['tortp.desktop'])]
     )
