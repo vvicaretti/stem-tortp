41f366d7a727575869de471def658a31f5f8deb0
boyska <piuttosto@logorroici.org>
2015-07-11 00:56:47 +0200

    includes missing files

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 setup(name='tortp',
-        version='0.4',
-        py_modules=['tortp'],
-        requires=['stem'],
-	scripts=['tortp-gtk'],
-	data_files=[('share/applications/', ['tortp.desktop'])]
-    )
+      version='0.4',
+      py_modules=['tortp'],
+      requires=['stem'],
+      scripts=['tortp-gtk', 'tortp'],
+      data_files=[('share/applications/', ['tortp.desktop']),
+                  ('share/pixmaps/', ['anonymous.ico'])]
+      )
