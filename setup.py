b771099dc27d58af4fe1daed123bff646d3155c4
vinc3nt <vinc3nt@riseup.net>
2015-07-13 16:01:40 +0100

    tortp version updated in tortp-gtk
    Signed-off-by: vinc3nt <vinc3nt@riseup.net>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='tortp',
-      version='0.4',
+      version='0.5',
       py_modules=['tortp'],
       requires=['stem'],
       scripts=['tortp-gtk', 'tortp'],
